97436200e401bc90c86dc5922141209e04375c05
sibirus <53700079+sibirus@users.noreply.github.com>
2020-04-28 20:59:35 +0900

    Add Bootstrap
    Add Bootstrap

--- simplecalculiator/src/main/java/com/simplecalculiator/simplecalculiator/GreetingController.java
+++ simplecalculiator/src/main/java/com/simplecalculiator/simplecalculiator/GreetingController.java
@@ -39,28 +39,3 @@ public class GreetingController
 
 }
 
-
-
-
-
-
-
-
-
-
-    /*
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    )
-    {
-        model.put("name", name);
-        return "greeting";
-    }
-
-    @GetMapping
-    public String main(Map<String, Object> model) {
-        model.put("some", "hello, letsCode!");
-        return "main";
-    }
-}*/
